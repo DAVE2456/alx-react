efc4b2cc2c2db7ad3c141a74af1c2bfedd389e1b
DAVE2456 <davehunegnaw@gmail.com>
2023-04-20 21:19:38 +0300

    coppy task 1

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,16 @@
+import $ from 'jquery'                                                          
+const _ = require('lodash');                                                    
+                                                                                
+$('body').append('<p>Holberton Dashboard</p>');                                 
+$('body').append('<p>Dashboard data for the students</p>');                     
+$('body').append('<p>Click here to get started</p>');                           
+$('body').append('<p id=\'count\'></p>');                                       
+$('body').append('<p>Copyright - Holberton School</p>');                        
+                                                                                
+let count = 0;                                                                                                                                                  
+function updateCounter() {                                                      
+    count++;                                                                    
+    $("#count").text(`${count} clicks on the button`);                          
+}                                                                               
+                                                                                
+$("button").on("click", _.debounce(updateCounter, 500));                        
