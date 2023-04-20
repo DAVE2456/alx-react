8d8070b3488524b5fe085223e3c42b397692aa5f
DAVE2456 <davehunegnaw@gmail.com>
2023-04-20 15:12:14 +0300

    correction code

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,10 +1,10 @@
-import $ from 'jquery';
-const _ from 'lodash';
+import $, { readyException } from 'jquery';
+const _ = require('lodash');
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<p>Click here to get started</p>');
-$('body').append('<p id='count'></p>'); 
+$('body').append('<p id=\'count\'></p>'); 
 $('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
