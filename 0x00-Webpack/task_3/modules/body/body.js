a8f64274ac367efd9d4a6d374372609fb36c2a18
DAVE2456 <davehunegnaw@gmail.com>
2023-04-21 13:57:00 +0300

    body js file

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+const _ = require('lodash');
+import style from './body.css'
+
+let btn = document.createElement('button');
+btn.text('Click here to get started');
+let p = document.createElement('p');
+p.id = 'count';
+
+$('body').append(btn, p);
