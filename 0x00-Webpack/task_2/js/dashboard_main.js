c62863f542b67f121781dc15fbbf08ffb3a178fe
DAVE2456 <davehunegnaw@gmail.com>
2023-04-21 12:33:55 +0300

    all file

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,13 +1,13 @@
-import $ from 'jquery';    
+import $ from 'jquery';
 const _ = require('lodash');
 import style from '../css/main.css';
 import image from '../assets/holberton-logo.jpg';
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Click here to get started</p>');
-$('body').append('<p id=\'count\'></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$('body').append("<p>Holberton Dashboard</p>");
+$('body').append("<p>Dashboard data for the students</p>");
+$('body').append("<p>Click here to get started</p>");
+$('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
 
 let count = 0;
 function updateCounter() {
