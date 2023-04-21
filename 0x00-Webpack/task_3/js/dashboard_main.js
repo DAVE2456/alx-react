4ef3e39727dd825896c8b64537928dd53a298695
DAVE2456 <davehunegnaw@gmail.com>
2023-04-21 15:22:32 +0300

    all task 3

---
+++ 0x00-Webpack/task_3/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+const _ = require('lodash');
+import style from '../css/main.css';
+import image from '../assets/holberton-logo.jpg'
+
+$('body').append("<p>Holberton Dashboard</p>");
+$('body').append("<p>Dashboard data for the students</p>");
+$('body').append("<p>Click here to get started</p>");
+$('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
+
+let count = 0;
+function updateCounter() {
+    count++;
+    $("#count").text(`${count} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
