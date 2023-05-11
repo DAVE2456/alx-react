a63da7d5b55c369da412b1601830a0b2cb6259fc
DAVE2456 <davehunegnaw@gmail.com>
2023-05-11 12:43:51 +0300

    correction code

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,14 +1,6 @@
+import { fromJs } from 'immutable'
+
 export default function accessImmutableObject(object, array) {
-  let value = object;
-  for (let key of array) {
-    if (value === undefined || value === null) {
-      return undefined;
-    }
-    if (typeof value.get === 'function') {
-      value = value.get(key);
-    } else {
-      value = value[key];
-    }
-  }
-  return value;
+  const mappedObj = fromJs(object);
+  return mappedObj.getIn(arry, undefined);
 }
