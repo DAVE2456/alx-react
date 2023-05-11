6c64e8ecbaa683e071a652e1b58f40b5956182a9
DAVE2456 <davehunegnaw@gmail.com>
2023-05-11 13:58:04 +0300

    initinal commit

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,11 @@
+import { List, Map } from 'immutable';
+
+export function concatElements(page1, page2) {
+  return List(page1.concat(page2));
+}
+
+export function mergeElements(page1, page2) {
+  const mergedObj = { ...page1, ...page2 };
+  const mergedMap = Map(mergedObj);
+  return List(mergedMap.values());
+}
