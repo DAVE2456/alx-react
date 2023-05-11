28264f6642e18898c600b5659c7052e3a02263b6
DAVE2456 <davehunegnaw@gmail.com>
2023-05-11 17:28:56 +0300

    correction code

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -7,5 +7,6 @@ export function concatElements(page1, page2) {
 export function mergeElements(page1, page2) {
   const mergedObj = { ...page1, ...page2 };
   const mergedMap = Map(mergedObj);
+
   return List(mergedMap.values());
 }
