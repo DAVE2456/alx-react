0c2fd768294536d13aa3c8705fdb1071179f1d91
DAVE2456 <davehunegnaw@gmail.com>
2023-05-11 17:30:27 +0300

    correction code

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,12 +1,15 @@
-import { List, Map } from 'immutable';
+import { Map, List } from 'immutable';
 
 export function concatElements(page1, page2) {
-  return List(page1.concat(page2));
+  const list1 = List(page1);
+  const list2 = List(page2);
+
+  return list1.concat(list2);
 }
 
 export function mergeElements(page1, page2) {
-  const mergedObj = { ...page1, ...page2 };
-  const mergedMap = Map(mergedObj);
+  const map1 = Map(page1);
+  const map2 = Map(page2);
 
-  return List(mergedMap.values());
+  return map1.merge(map2);
 }
